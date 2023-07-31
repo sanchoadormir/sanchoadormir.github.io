047b2841020674cd61685c30dc04a5b521c78647
sanchoadormir <78618224+sanchoadormir@users.noreply.github.com>
2023-07-31 18:37:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+  document.getElementsByClassName("menucontent").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.menubutton')) {
+    var dropdowns = document.getElementsByClassName("menulink");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
