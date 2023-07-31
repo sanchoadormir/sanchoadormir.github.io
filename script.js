0ae659b0f98f16ea2d16a3b0f90764efcc7521bb
sanchoadormir <78618224+sanchoadormir@users.noreply.github.com>
2023-07-31 19:07:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,17 +33,3 @@ toggle between hiding and showing the dropdown content */
 function dropdown() {
   document.getElementById("menucont").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.menubutton')) {
-    var dropdowns = document.getElementsByClassName("menulink");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
